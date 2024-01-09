c85d7cccc1accb9bb1f1f87fefd2c177185aea53
Nico Spiller <nspiller1@mailbox.org>
2024-01-09 13:55:08 -0500

    added score comparison

---
+++ scripts/batch_compare.py
@@ -0,0 +1,107 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: ml
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+
+from pathlib import Path
+from src import batch_helpers as bh
+from src import visualization as vis
+import seaborn as sns
+
+
+# %% [markdown]
+# # Load scores
+#
+# Here we load scores from the ridge regression predictions, 
+# which are stored in the `pred_ridge_scores.csv` files, into one big dataframe.
+# Note that this selection inclues all recordings under the `p_root`.
+
+# %%
+# select scores for each paramter set
+p_root = Path(r'C:\temp\dual_ephys')
+p_scores = [ *p_root.glob('**/pred_ridge_scores.csv') ]
+scores = bh.load_scores(p_scores)
+scores
+
+# %% [markdown]
+# # Compare parameter sets
+#
+# Here we compare the parameter sets `params_1` and `params_2` that we created in the previous notebook.
+# For this we select a subste of the scores:
+# - only parameter sets `params_1` and `params_2`
+# - only epoch `all`
+# - only probes `ALM_STR`
+#
+#
+# The two parameter sets differ only in the `subtract_baseline` parameter.
+# We add an additional column to the `scores` dataframe to use as hue in plotting.
+#
+# Note, that to compare parameter sets for just one animal/recording,
+# choose `p_root` accordingly, or pass e.g. `col='animal'` to the plotting function.
+
+# %%
+# subset scores dataframe
+idx1 = scores.loc[:, 'settings'].isin(['params_1', 'params_2'])
+idx2 = scores.loc[:, 'epoch'].eq('all')
+idx3 = scores.loc[:, 'probes'].eq('ALM_STR')
+df = scores.loc[idx1 & idx2 & idx3, :].copy()
+
+# create new column `subtract_baseline`
+d = {'params_1': 'yes', 'params_2': 'no'}
+df.loc[:, 'subtract_baseline'] = df.loc[:, 'settings'].map(d)
+
+# plot
+vis.plot_box_and_points(df, x='interaction', y='score', hue='subtract_baseline')
+
+# %% [markdown]
+# # Compare epochs
+#
+# To compare epochs, we select a subset of the scores:
+# - only parameter set `params_1`
+# - only epochs defined in `l_epochs`
+# - only probes `ALM_STR`
+#
+# When we pass `hue_order=l_epochs` to the plotting function, we can define their oder
+# in the plot.
+
+# %%
+# subset scores dataframe
+l_epochs = ['all', 'pre_lick', 'post_lick']
+idx1 = scores.loc[:, 'settings'].eq('params_1')
+idx2 = scores.loc[:, 'epoch'].isin(l_epochs)
+idx3 = scores.loc[:, 'probes'].eq('ALM_STR')
+df = scores.loc[idx1 & idx2 & idx3, :].copy()
+
+# plot
+vis.plot_box_and_points(df, x='interaction', y='score', hue='epoch', hue_order=l_epochs)
+
+# %% [markdown]
+# # Compare sessions
+#
+# To compare sessions, we select a subset of the scores:
+# - only parameter set `params_1`
+# - only epoch `all`
+
+# %%
+# subset scores dataframe
+idx1 = scores.loc[:, 'settings'].eq('params_1')
+idx2 = scores.loc[:, 'epoch'].eq('all')
+df = scores.loc[idx1 & idx2, :].copy()
+
+# plot
+vis.plot_box_and_points(df, x='probes', y='score', hue='animal')
--- src/visualization.py
+++ src/visualization.py
@@ -3,11 +3,13 @@ import pandas as pd
 
 import matplotlib.pylab as plt
 import seaborn as sns
-plt.rcParams['savefig.facecolor'] = 'w'
+
+plt.rcParams["savefig.facecolor"] = "w"
 sns.set_style("whitegrid")
 
-def plot_rate_dist(X, Y, path=''):
-    'plot rate distributions for two populations'
+
+def plot_rate_dist(X, Y, path=""):
+    "plot rate distributions for two populations"
 
     fig, ax = plt.subplots()
 
@@ -16,69 +18,75 @@ def plot_rate_dist(X, Y, path=''):
 
     sns.histplot(
         ax=ax,
-        data={f'X (n = {len(r1)})': r1, f'Y (n = {len(r2)})': r2}, 
-        binwidth=1, stat='density', common_norm=False)
-    ax.set_xlabel('avg firing rate [Hz]')
-    
+        data={f"X (n = {len(r1)})": r1, f"Y (n = {len(r2)})": r2},
+        binwidth=1,
+        stat="density",
+        common_norm=False,
+    )
+    ax.set_xlabel("avg firing rate [Hz]")
+
     fig.tight_layout()
     if path:
         fig.savefig(path)
         plt.close(fig)
 
-def plot_gridsearch(mods, param, other_mods=dict(), logscale=True, path=''):
-    'Plot hyperparameter search results and compare to other (best) models'
+
+def plot_gridsearch(mods, param, other_mods=dict(), logscale=True, path=""):
+    "Plot hyperparameter search results and compare to other (best) models"
 
     df = pd.DataFrame(mods.cv_results_)
-    ncv = len([c for c in df.columns if c.startswith('split')])
-    x = [ np.array(*i.values()).item() for i in df.loc[:, 'params'] ]
-    y, yerr = df.loc[:, 'mean_test_score'], df.loc[:, 'std_test_score'] / np.sqrt(ncv)
+    ncv = len([c for c in df.columns if c.startswith("split")])
+    x = [np.array(*i.values()).item() for i in df.loc[:, "params"]]
+    y, yerr = df.loc[:, "mean_test_score"], df.loc[:, "std_test_score"] / np.sqrt(ncv)
 
     fig, ax = plt.subplots()
     ax.errorbar(x, y, yerr, label=param)
-    ax.axvline(x[y.argmax()], c='gray', ls=':')
+    ax.axvline(x[y.argmax()], c="gray", ls=":")
 
     for i, (name, mods) in enumerate(other_mods.items()):
         df = pd.DataFrame(mods.cv_results_)
-        ncv = len([c for c in df.columns if c.startswith('split')])
-        i_max = df.loc[:, 'mean_test_score'].argmax()
+        ncv = len([c for c in df.columns if c.startswith("split")])
+        i_max = df.loc[:, "mean_test_score"].argmax()
         ds = df.loc[i_max, :]
-        x = np.array(*ds.loc['params'].values()).item()
-        y, yerr = ds.loc['mean_test_score'], ds.loc['std_test_score'] / np.sqrt(ncv)
-        ax.axhline(y, lw=1, label=name, c=f'C{i+1}')
-        ax.axhline(y+yerr, ls=':', lw=1, c=f'C{i+1}')
-        ax.axhline(y-yerr, ls=':', lw=1, c=f'C{i+1}')
+        x = np.array(*ds.loc["params"].values()).item()
+        y, yerr = ds.loc["mean_test_score"], ds.loc["std_test_score"] / np.sqrt(ncv)
+        ax.axhline(y, lw=1, label=name, c=f"C{i+1}")
+        ax.axhline(y + yerr, ls=":", lw=1, c=f"C{i+1}")
+        ax.axhline(y - yerr, ls=":", lw=1, c=f"C{i+1}")
 
     ax.legend()
-    ax.set_title(f'best score: {y.max():1.2f}')
-    ax.set_xlabel('parameter')
-    ax.set_ylabel('score')
+    ax.set_title(f"best score: {y.max():1.2f}")
+    ax.set_xlabel("parameter")
+    ax.set_ylabel("score")
 
     if logscale:
-        ax.set_xscale('log')
+        ax.set_xscale("log")
 
     fig.tight_layout()
     if path:
         fig.savefig(path)
         plt.close(fig)
 
-def plot_missing(df_psth, bin_size, vmax=None, path=''):
 
-    df = pd.pivot_table(data=df_psth, values='fr', index='unit', columns='T')
+def plot_missing(df_psth, bin_size, vmax=None, path=""):
+    df = pd.pivot_table(data=df_psth, values="fr", index="unit", columns="T")
     # df = df.apply(lambda x: x / np.mean(x), axis=1)
 
     n_unt, n_bins = df.shape
-    fig, axarr = plt.subplots(ncols=2, width_ratios=(3, 1), figsize=(n_bins / 0.5e4, n_unt / 10))
+    fig, axarr = plt.subplots(
+        ncols=2, width_ratios=(3, 1), figsize=(n_bins / 0.5e4, n_unt / 10)
+    )
 
     ax = axarr[0]
     im = ax.pcolormesh(df.columns * bin_size, df.index, df.values, vmax=vmax)
-    
-    label = 'firing rate [Hz]'
+
+    label = "firing rate [Hz]"
     if vmax:
-        label = label + f' (capped at {vmax})'
-        
-    fig.colorbar(im, ax=ax, location='right', orientation='vertical', label=label)
-    ax.set_xlabel('time [s]')
-    ax.set_ylabel('unit')
+        label = label + f" (capped at {vmax})"
+
+    fig.colorbar(im, ax=ax, location="right", orientation="vertical", label=label)
+    ax.set_xlabel("time [s]")
+    ax.set_ylabel("unit")
     # for b in B:
     #     ax.axvline(b * bin_size, lw=.1, c='gray')
 
@@ -88,33 +96,34 @@ def plot_missing(df_psth, bin_size, vmax=None, path=''):
     ax.barh(y, x)
     ax.margins(y=0)
     ax.set_xlim((0, 100))
-    ax.set_ylabel('unit')
-    ax.set_xlabel('missing [%]')
+    ax.set_ylabel("unit")
+    ax.set_xlabel("missing [%]")
 
     fig.tight_layout()
     if path:
         fig.savefig(path)
         plt.close(fig)
 
-def plot_unit(rec, bin_size, unit, xlims=(None, None), path=''):
 
-
+def plot_unit(rec, bin_size, unit, xlims=(None, None), path=""):
     fig, ax = plt.subplots(figsize=(20, 5))
 
-    d = rec.df_prec.groupby('unit').get_group(unit)
-    x = d.loc[:, 'T'].values * bin_size
-    y = d.loc[:, 'fr'].values
+    d = rec.df_prec.groupby("unit").get_group(unit)
+    x = d.loc[:, "T"].values * bin_size
+    y = d.loc[:, "fr"].values
 
-    rec.df_trl.loc[:, 'Tf'] = np.cumsum(rec.df_trl.loc[:, 'dtf'] - rec.df_trl.loc[:, 'dt0'])
-    for trl in d.loc[:, 'trial'].unique():
-        tf = rec.df_trl.groupby('trial').get_group(trl).loc[:, 'Tf'].item()
-        ax.axvline(tf, c='gray', lw=.5)
+    rec.df_trl.loc[:, "Tf"] = np.cumsum(
+        rec.df_trl.loc[:, "dtf"] - rec.df_trl.loc[:, "dt0"]
+    )
+    for trl in d.loc[:, "trial"].unique():
+        tf = rec.df_trl.groupby("trial").get_group(trl).loc[:, "Tf"].item()
+        ax.axvline(tf, c="gray", lw=0.5)
 
     ax.plot(x, y)
     ax.margins(x=0)
 
-    ax.set_xlabel('time [s]')
-    ax.set_ylabel('firing rate [Hz]')
+    ax.set_xlabel("time [s]")
+    ax.set_ylabel("firing rate [Hz]")
     ax.set_xlim(xlims)
 
     fig.tight_layout()
@@ -122,8 +131,9 @@ def plot_unit(rec, bin_size, unit, xlims=(None, None), path=''):
         fig.savefig(path)
         plt.close(fig)
 
-def plot_mean_response(df_bin, arr_pred=None, scores={}, path=''):
-    'Plot PSTH for all units in `df_bin` and optionally add predictions in `arr_pred`'
+
+def plot_mean_response(df_bin, arr_pred=None, scores={}, path=""):
+    "Plot PSTH for all units in `df_bin` and optionally add predictions in `arr_pred`"
 
     if arr_pred is not None:
         df_bin_pred = df_bin.copy()
@@ -133,31 +143,34 @@ def plot_mean_response(df_bin, arr_pred=None, scores={}, path=''):
     nc = 5
     nr = int(np.ceil(nu / nc))
 
-    fig, axmat = plt.subplots(ncols=nc, nrows=nr, figsize=(3*nc, 2*nr), squeeze=False)
+    fig, axmat = plt.subplots(
+        ncols=nc, nrows=nr, figsize=(3 * nc, 2 * nr), squeeze=False
+    )
 
     for ax, u in zip(axmat.flatten(), df_bin.columns):
-
         df = df_bin.loc[:, u].reset_index()
-        df.loc[:, 'type'] = 'true'
+        df.loc[:, "type"] = "true"
         if arr_pred is not None:
             df_pred = df_bin_pred.loc[:, u].reset_index()
-            df_pred.loc[:, 'type'] = 'pred'
+            df_pred.loc[:, "type"] = "pred"
             df = pd.concat([df, df_pred])
 
-        sns.lineplot(ax=ax, data=df, x='bin', y=u, hue='type', errorbar='sd', legend=False)
+        sns.lineplot(
+            ax=ax, data=df, x="bin", y=u, hue="type", errorbar="sd", legend=False
+        )
 
-        title = f'unit {u}'
+        title = f"unit {u}"
         if scores:
-            title += f' ({scores[u]:1.3f})'
+            title += f" ({scores[u]:1.3f})"
         ax.set_title(title)
         ax.margins(x=0)
-        ax.set_xlabel('')
-        ax.set_ylabel('')
+        ax.set_xlabel("")
+        ax.set_ylabel("")
 
     for ax in axmat[-1]:
-        ax.set_xlabel('time from cue [s]')
+        ax.set_xlabel("time from cue [s]")
     for ax in axmat.T[0]:
-        ax.set_ylabel('firing rate [Hz]')
+        ax.set_ylabel("firing rate [Hz]")
 
     for ax in axmat.flatten():
         if not ax.has_data():
@@ -168,47 +181,102 @@ def plot_mean_response(df_bin, arr_pred=None, scores={}, path=''):
         fig.savefig(path)
         plt.close(fig)
 
-def _barplot_wrapper(df, col, ax):
 
+def _barplot_wrapper(df, col, ax):
     # plot trial type counts
-    sns.histplot(data=df, y=col, ax=ax, stat='percent', multiple='stack', shrink=.8)
+    sns.histplot(data=df, y=col, ax=ax, stat="percent", multiple="stack", shrink=0.8)
 
     # absolute and relative counts as labels
     c = ax.containers[0]
     c_rel = c.datavalues
     c_abs = c_rel * len(df) / 100
-    l = [ f' {int(i):d} ({j:1.1f} %)' for i, j in zip(c_abs, c_rel) ]
+    l = [f" {int(i):d} ({j:1.1f} %)" for i, j in zip(c_abs, c_rel)]
     ax.bar_label(c, labels=l)
     ax.set_xlim(0, 100)
-    ax.set_xlabel('Percentage of trials')
+    ax.set_xlabel("Percentage of trials")
 
     return ax
 
-def plot_trial_infos(df_trl, path=''):
-    'Plot trial type, response, and lick time distributions'
+
+def plot_trial_infos(df_trl, path=""):
+    "Plot trial type, response, and lick time distributions"
 
     df = df_trl.copy()
-    df.loc[:, 'short'] = df.loc[:, 'trial_type'].str.split('_').str[:2].str.join('_')
+    df.loc[:, "short"] = df.loc[:, "trial_type"].str.split("_").str[:2].str.join("_")
 
     # plot
-    fig, axarr = plt.subplots(figsize=(6, 9), nrows=3)
+    fig, axmat = plt.subplots(figsize=(12, 6), nrows=2, ncols=2)
+    axarr = axmat.T.flatten()
 
     ax = axarr[0]
-    ax.set_title('trial type distribution')
-    _barplot_wrapper(df, 'short', ax)
-    ax.set_ylabel('Trial type')
+    ax.set_title("trial type distribution")
+    _barplot_wrapper(df, "short", ax)
+    ax.set_ylabel("Trial type")
 
     ax = axarr[1]
-    ax.set_title('trial response distribution')
-    _barplot_wrapper(df, 'response', ax)
-    ax.set_ylabel('Trial response')
+    ax.set_title("trial response distribution")
+    _barplot_wrapper(df, "response", ax)
+    ax.set_ylabel("Trial response")
 
     ax = axarr[2]
-    ax.set_title('lick time distribution')
-    sns.histplot(data=df, x='dt_lck', hue='response', ax=ax, multiple='layer', binwidth=.2)
-    ax.set_xlabel('lick time relative to cue (s)')
+    ax.set_title("lick time distribution")
+    sns.histplot(
+        data=df, x="dt_lck", hue="response", ax=ax, multiple="layer", binwidth=0.2
+    )
+    ax.set_xlabel("lick time relative to cue (s)")
+
+    ax = axarr[3]
+    ax.set_title("lick groups")
+    sns.histplot(data=df, x="lick_group", ax=ax, hue="response", multiple="stack", shrink=0.8)
 
     fig.tight_layout()
     if path:
         fig.savefig(path)
         plt.close(fig)
+
+
+def plot_box_and_points(df, x, y, col=None, hue=None, order=[], hue_order=[], ylim=(-1, 1), path=""):
+    "Plot boxplots with points on top"
+
+    if not hue_order:
+        hue_order = sorted(df.loc[:, hue].unique())
+    if not order:
+        order = sorted(df.loc[:, x].unique())
+
+    g = sns.catplot(
+        df,
+        x=x,
+        y=y,
+        col=col,
+        hue=hue,
+        order=order,
+        hue_order=hue_order,
+        kind="box",
+        whis=0,
+        fliersize=0,
+        palette="pastel",
+        sharex=False,
+        facet_kws={"ylim": ylim},
+    )
+
+    g.map(
+        sns.stripplot,
+        data=df,
+        x=x,
+        y=y,
+        hue=hue,
+        order=order,
+        hue_order=hue_order,
+        dodge=True,
+        palette="deep",
+        edgecolor="auto",
+        linewidth=0.5,
+        size=1,
+    )
+
+    # fig = g.fig
+    # fig.tight_layout()
+    # if path:
+    #     fig.savefig(path)
+    #     plt.close(fig)
+
